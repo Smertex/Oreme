e0c6c0dad02fa7934898092308664db8d060b1b6
Smertex <kiratsvetkova102@gmail.com>
2024-09-27 00:12:40 +0300

    Refactoring

--- src/main/java/by/smertex/realisation/session/TransactionBasicRealisation.java
+++ src/main/java/by/smertex/realisation/session/TransactionBasicRealisation.java
@@ -7,8 +7,8 @@ import by.smertex.interfaces.session.Transaction;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class TransactionBasicRealisation implements Transaction {
 
@@ -16,13 +16,13 @@ public class TransactionBasicRealisation implements Transaction {
 
     private final Connection connection;
 
-    private List<String> sqlQueries;
+    private BlockingQueue<String> sqlQueries;
 
     @Override
     public void begin() {
         if(session == null || connection == null)
             throw new TransactionException(new RuntimeException());
-        sqlQueries = new ArrayList<>();
+        sqlQueries = new LinkedBlockingQueue<>();
     }
 
     @Override
@@ -39,10 +39,10 @@ public class TransactionBasicRealisation implements Transaction {
     public void commit(){
         try {
             Statement statement = connection.createStatement();
-            for(String sql: sqlQueries) statement.execute(sql);
+            for(int i = 0; i < sqlQueries.size(); i++) statement.execute(sqlQueries.take());
             connection.commit();
             deleteThisInstance();
-        } catch (SQLException e) {
+        } catch (SQLException | InterruptedException e) {
             throw new TransactionException(e);
         }
     }
