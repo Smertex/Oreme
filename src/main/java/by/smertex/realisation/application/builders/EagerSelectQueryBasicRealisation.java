2148731cb947561f7dcdb716059cba74d75d7a6d
Smertex <kiratsvetkova102@gmail.com>
2024-10-07 23:06:14 +0300

    Moving away from the Proxy past. Refactoring

--- src/main/java/by/smertex/realisation/application/builders/EagerSelectQueryBasicRealisation.java
+++ src/main/java/by/smertex/realisation/application/builders/EagerSelectQueryBasicRealisation.java
@@ -11,7 +11,6 @@ import by.smertex.interfaces.cfg.EntityManager;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public abstract class EagerSelectQueryBasicRealisation extends AbstractQueryBuilderBasicRealisation implements QueryBuilder, EntityCollector {
