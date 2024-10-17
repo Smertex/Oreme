2d2895b3ff0022efe4ea31b3293d0b3c6c7184ac
Smertex <kiratsvetkova102@gmail.com>
2024-10-18 02:15:24 +0300

    Optimization. Cache changes. Refactoring

--- src/main/java/by/smertex/interfaces/application/session/Session.java
+++ src/main/java/by/smertex/interfaces/application/session/Session.java
@@ -1,13 +1,8 @@
 package by.smertex.interfaces.application.session;
 
-import by.smertex.exceptions.application.SessionException;
 import by.smertex.realisation.elements.IsolationLevel;
 
 import java.io.Closeable;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public interface Session extends Closeable, Crud {
 
